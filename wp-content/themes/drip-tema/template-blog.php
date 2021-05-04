03873afdadc5a9a24b87d008a1a3d7f25cb0a8fc
Ryan-Phillips Cornelio <ryan.cornelio@gmail.com>
2021-05-04 13:06:29 +0200

    Add background color, center text, changed font

---
+++ wp-content/themes/drip-tema/template-blog.php
@@ -0,0 +1,51 @@
+<?php
+
+/* 
+Template name: Blog
+*/
+get_header();
+?>
+
+<div class ="main-blog">
+    <div class ="blogpost-wrapper">
+    <?php
+$args = array(
+
+    'post_type' => 'post',
+
+    'posts_per_page' => -1
+
+    );
+
+$loop = new WP_Query( $args );
+
+if ( $loop->have_posts() ) {
+
+    while ( $loop->have_posts() ) : $loop->the_post();
+        ?>
+        <h2>
+        <?php
+        the_title();
+        ?>
+        </h2>
+        <p>
+        <?php
+        the_excerpt();
+        ?>
+        </p>
+    <?php
+endwhile;
+
+} else {
+
+    echo __( 'No posts found' );
+
+}
+
+wp_reset_postdata();
+    ?>
+    </div>
+</div>
+<?php
+get_footer();
+?>

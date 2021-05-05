0d7be27437a693f43fb95e825f8035d0e3930c55
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-05-05 14:38:35 +0200

    custom-meta-box

--- wp-content/themes/drip-tema/template-stores.php
+++ wp-content/themes/drip-tema/template-stores.php
@@ -1,42 +1,36 @@
 <?php 
-/**
-* Template Name: Stores-page
+/* 
+Template name: store
 */
 get_header();
+echo '<div class="main-stores">';
+echo '<hr>';
+$args = array(
+    'post_type' => 'stores',
+    'posts_per_page' => -1
+    );
+$loop = new WP_Query( $args );
+if ( $loop->have_posts() ) {
+    while ( $loop->have_posts() ) : $loop->the_post();
+        echo '<div class="stores-wrapper">';
+        echo '<h2>';    
+        echo the_title();
+        echo '</h2>';
+        $imgURL = get_the_post_thumbnail_url();
+        echo '<div class="img-stores-wrapper">';
+        echo '<img class="img-stores" src="'.$imgURL.'">';
+        echo '</div>';
+        echo '<p>';
+        echo the_content();
+        echo '</p>';
+        echo 'Address: ' . get_post_meta( get_the_ID(), '_wporg_meta_key', true );
+        echo '</div>';
+        echo '<hr>';
+    endwhile;
+} else {
+    echo __( 'No products found' );
+}
+wp_reset_postdata();
 
-
-	echo '<div class="main-stores">';
-    echo '<hr>';
-    $args = array(
-        'post_type' => 'stores',
-        'posts_per_page' => -1
-        );
-    $loop = new WP_Query( $args );
-    if ( $loop->have_posts() ) {
-        while ( $loop->have_posts() ) : $loop->the_post();
-            echo '<div class="stores-wrapper">';
-           /*  $link_store = get_permalink();
-            echo '<a href="'.$link_store.'">';
-            echo the_title();
-            echo '</a>'; */
-            echo '<h2>';
-            echo the_title();
-            echo '</h2>';
-            echo '<p>';
-            echo the_excerpt();
-            echo '</p>';
-            /* echo the_post_thumbnail(); */
-            $imgURL = get_the_post_thumbnail_url();
-            echo '<div class="img-stores-wrapper">';
-            echo '<img class="img-stores" src="'.$imgURL.'">';
-            echo '</div>';
-            echo '</div>';
-            echo '<hr>';
-        endwhile;
-    } else {
-        echo __( 'No products found' );
-    }
-    wp_reset_postdata();
-    
-    echo '</div>';
+echo '</div>'; 
 get_footer();

3ac701371c078787e84ad53c3409dc1dc80ff232
elinln <linneaelinnilsson@gmail.com>
2021-05-11 14:46:44 +0200

    footer things

--- wp-content/themes/drip-tema/footer.php
+++ wp-content/themes/drip-tema/footer.php
@@ -1,5 +1,5 @@
 <footer class="footergrp3">
-    <div class="footerwidget">
+    <div style="font-size: 12px;" class="footerwidget">
     <?php dynamic_sidebar('footerwidget'); ?>
     </div>
     <div class="footermenu">
@@ -12,7 +12,15 @@
 		?>
     </div>
 
+    
 </footer>
+<div class="finishingFooter">
+<p style="font-size: 12px; padding-right: 100px; padding-left: 100px;">Here at YOULRY we love everything that has to do with jewelry and beauty, so we make sure we are always up to date with the season’s latest trends. We want to inspire you with new arrivals every day, so that you will easily be able to find a look that suits you perfectly. Here you will find everything from trendy watches to necklaces and bracelets for all sorts of occasions – always for a good price and with fast deliveries.</p>
+
+<p style="font-size: 7px; padding: 10px;">
+ -------------------------------------------------------------- YOULRY AB © --------------------------------------------------------------</p>
+ <p style="font-size: 7px;"> Org-number: 009754280470987</p>
+</div>
 
 <?php 
     wp_footer(); 
